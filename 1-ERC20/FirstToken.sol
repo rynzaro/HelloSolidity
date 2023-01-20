94cc2f7ac07cccfa2375387868a0a4a3afb00eed
rynzaro <rynzaro44@gmail.com>
2023-01-20 11:15:51 +0100

    Create "FirstToken" ERC20-Token

--- 1-ERC20/FirstToken.sol
+++ 1-ERC20/FirstToken.sol
@@ -10,6 +10,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract FirstToken is ERC20("FirstToken", "FTK") {
-
+contract FirstToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("FirstToken", "FTK") {
+        _mint(msg.sender, initialSupply);
+    }
 }
