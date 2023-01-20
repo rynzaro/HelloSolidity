d9cefcd1daf914df144f025bccecffbcde805c1e
EfteYo <58327339+EfteYo@users.noreply.github.com>
2023-01-20 15:26:04 +0100

    Delete second ERC20 token trial

--- 1-ERC20/FirstToken.sol
+++ 1-ERC20/FirstToken.sol
@@ -1,14 +1,10 @@
-pragma solidity 0.8.17;
-
-
 // contracts/OceanToken.sol
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
 
 contract FirstToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("FirstToken", "FTK") {
